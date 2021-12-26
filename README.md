a1ab75f3e43dc46f4ed10d30820cb0c87170c391
Matthew Li <matthew.elbert.li@gmail.com>
2021-12-26 13:27:32 -0800

    Add extension source and README

---
+++ README.md
@@ -0,0 +1,10 @@
+# Chrome Extension: Mandarin Daily Keyboard Shortcuts
+
+A Chrome extension that adds keyboard shortcuts for navigating Tweets by [@mandarin_daily](https://twitter.com/mandarin_daily):
+
+| Key | Function |
+| --- | -------- |
+| `w` | Navigate to Last Week's Tweet. |
+| `s` | Navigate to Last Month's Tweet. |
+| `a` | Navigate to the Random previous Tweet. |
+| `q` | Go back one page in history. |
---
+++ extension/assets/js/content_script.js
@@ -0,0 +1,63 @@
+/**
+ * Add keyboard shortcuts accessible from Tweets by the Twitter account.
+ * "w": Last Week
+ * "s": Last Month
+ * "a": Random ("r" triggers reply)
+ * "q": Back ("b" triggers block)
+ * @return {null}
+ */
+ document.addEventListener("keyup", function (event) {
+  let tweetPrefix = `https://twitter.com/${USERNAME}/status/`;
+  if (window.location.href.startsWith(tweetPrefix)) {
+    switch(event.key) {
+      case "w":
+        navigateToTweet(TweetLinkType.LAST_WEEK);
+        break;
+      case "s":
+        navigateToTweet(TweetLinkType.LAST_MONTH);
+        break;
+      case "a":
+        navigateToTweet(TweetLinkType.RANDOM);
+        break;
+      case "q":
+        history.back();
+        break;
+    }
+  }
+});
+
+
+/**
+ * The username of the Twitter account.
+ * @const {string}
+ */
+ const USERNAME = "mandarin_daily";
+
+ /**
+  * Types of links in the body of a Tweet.
+  * @enum {string}
+  */
+ const TweetLinkType = {
+   LAST_WEEK: "Last Week",
+   LAST_MONTH: "Last Month",
+   RANDOM: "Random",
+ };
+
+
+/**
+ * Navigate to the Tweet having the given type, which should be
+ * from TweetLinkType, if possible.
+ * @return {null}
+ */
+function navigateToTweet(tweetLinkType) {
+  const hrefPrefix = `/${USERNAME}/statuses/`;
+  const selector = `a[href^="${hrefPrefix}"]`
+  let anchors = document.querySelectorAll(selector);
+  for (let anchor of anchors) {
+    let labelText = anchor.previousSibling.textContent;
+    if (labelText.includes(tweetLinkType)) {
+      anchor.click();
+      return;
+    }
+  }
+}
